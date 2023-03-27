5dde974d1c8175164156bd24a8a2ca1284b4c9ed
voidzeronull0 <user@omen.localdomain>
2023-03-28 02:43:20 +0300

    Third commit!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include "glfw.hpp"
+#include "entt.hpp"
 
-int main()
+int main(void)
 {
-	std::cout << "Hello world!";
+    entt::entity AAAAA;
+    GLFWwindow* window;
 
-	return 0;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
