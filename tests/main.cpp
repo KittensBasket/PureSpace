53dc8f9c8bfd450073d0fd86aeba0aabc0c1ba60
riogor <astronaft100@yandex.ru>
2023-07-19 19:51:54 +0300

    Fix GPU tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,12 @@
 #include "gtest/gtest.h"
 
 #include "collision_test.hpp"
-// #include "shader_test.hpp"
-// #include "graphics_test.hpp"
+#include "graphics_test.hpp"
+#include "shader_test.hpp"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	glfwSetErrorCallback([](int error, const char *description) { throw OpenglError(description); });
+
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
